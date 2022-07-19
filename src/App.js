dbf08158829f2ae239f5e707d2b9ae634de45a52
Lucas.M <lucass8v@hotmail.com>
2022-07-19 11:14:55 -0300

    Finished first screen static layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import HomePage from "./components/HomePage"
 export default function App() {
     return (
         <>
-            <div className="homePage">
-                <img src="./assets/logo.png" alt="Lightning bolt" />
-                <h1>ZapRecall</h1>
-                <button> Iniciar Recall!</button>
+            {/* <HomePage/> */}
+            <div className="zapPage">
+                <header>
+                    <img src="./assets/logo.png" alt="Lightning bolt" />
+                    <h1>ZapRecall</h1>
+                </header>
+                <div className="zaps">
+                    <div className="zapCard">
+                        <p>Pergunta 1</p>
+                        <ion-icon name="play-outline"></ion-icon>
+                    </div>
+                    <div className="zapCard">
+                        <p>Pergunta 1</p>
+                        <ion-icon name="play-outline"></ion-icon>
+                    </div>
+                    <div className="zapCard">
+                        <p>Pergunta 1</p>
+                        <ion-icon name="play-outline"></ion-icon>
+                    </div>
+                    <div className="zapCard">
+                        <p>Pergunta 1</p>
+                        <ion-icon name="play-outline"></ion-icon>
+                    </div>
+                </div>
+                <footer>
+                    <p>0/4 Concluidos</p>
+                </footer>
             </div>
+
         </>
     )
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div className="homePage">
+            <img src="./assets/logo.png" alt="Lightning bolt" />
+            <h1>ZapRecall</h1>
+            <button> Iniciar Recall!</button>
+        </div>
+    )
+}

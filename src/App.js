21f6a714db1ecaf391fd3a592c030ecfe668b766
Lucas.M <lucass8v@hotmail.com>
2022-07-19 10:17:26 -0300

    Finished login page static layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+    return (
+        <>
+            <div className="homePage">
+                <img src="./assets/logo.png" alt="Lightning bolt" />
+                <h1>ZapRecall</h1>
+                <button> Iniciar Recall!</button>
+            </div>
+        </>
+    )
+}
+
+
